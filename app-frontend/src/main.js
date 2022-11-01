c53a8a67f41862568deb72955751b9a18c2cb855
unknown <nguyenphiikhanh@gmail.com>
2022-11-01 16:48:47 +0700

    #frontend-config add assets

--- app-frontend/src/main.js
+++ app-frontend/src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+//bootstrap 5
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 createApp(App).mount('#app')
