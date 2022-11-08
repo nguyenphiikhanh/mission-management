edc1afee6b0189a56d9dc3c931818797c0f27020
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-11-08 11:42:13 +0700

    #frontend-config conf: vite config

--- app-frontend/vite.config.js
+++ app-frontend/vite.config.js
@@ -1,5 +1,11 @@
-const { createVuePlugin } = require('vite-plugin-vue2');
+import {createVuePlugin} from "vite-plugin-vue2";
+import { fileURLToPath } from 'url';
 
 module.exports = {
   plugins: [createVuePlugin()],
+  resolve:{
+    alias:{
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
 };
